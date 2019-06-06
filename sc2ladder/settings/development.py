05c9403ccfcba6d9333357491ed544b5509e9eb7
Tristan Ohlson <tsohlson@gmail.com>
2019-06-06 16:01:48 -0400

    Remove search.js and add production settings

---
+++ sc2ladder/settings/development.py
@@ -0,0 +1,14 @@
+from .common import *
+
+SECRET_KEY = '0c(i&aa$a5gt8#2mc&#7mw7mw_59vy3@$or#fphd6tbf2esjjo'
+DEBUG = True
+
+DB_BATCH_SIZE = 100
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = []
---
+++ sc2ladder/settings/production.py
@@ -0,0 +1,34 @@
+import urllib.parse as urlparse
+
+from .common import *
+
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
+
+DB_BATCH_SIZE = 1000
+url = urlparse.urlparse(os.environ['DATABASE_URL'])
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
