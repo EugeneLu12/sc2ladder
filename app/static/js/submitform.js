682973044c3ab577322513159538155c7e4ba967
Eugene Lu <e6lu@edu.uwaterloo.ca>
2019-06-08 03:43:11 -0700

    properly encoded url

--- app/static/js/submitform.js
+++ app/static/js/submitform.js
@@ -1,13 +1,13 @@
 function make_request() { 
     let queryField = document.getElementById('queryField').value;
     let limitField = document.getElementById('limitField').value;
-    //const http = new XMLHttpRequest();
-    const url = `${window.location.origin}/api/player?query=${queryField}&limit=${limitField}`
     const http = new XMLHttpRequest();
+    const queryUrl = encodeURIComponent(`${queryField}`)
+    const limitUrl = encodeURIComponent(`${limitField}`)
+    url = `${window.location.origin}/api/player?query=${queryUrl}&limit=${limitUrl}`
     http.open('GET', url);
     http.send();
     http.onreadystatechange=(e)=> {
-        console.log(http.responseText)
         parsedResponseJSON = JSON.stringify(JSON.parse(http.responseText), undefined, 2);
         document.getElementById('playerResponse').innerHTML = parsedResponseJSON;
     }
