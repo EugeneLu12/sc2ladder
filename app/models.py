b74bedd966c44efa2fc4790ac9840c8c7ff0d28a
jason <jteague9@gatech.edu>
2019-06-12 19:02:42 -0700

    Added twitch field to Identity model.

--- app/models.py
+++ app/models.py
@@ -25,6 +25,7 @@ class Identity(models.Model):
     alias = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=30, null=True, blank=True)
+    twitch = models.CharField(max_length=30, null=True, blank=True)
     created_at: datetime = models.DateTimeField(auto_now_add=True)
     modified_at: datetime = models.DateTimeField(auto_now=True)
 
