3afbc0eec02937266327f174e22bc185b1c7214a
jason <jteague9@gatech.edu>
2019-06-12 18:20:17 -0700

    Added search fields for admin pages. Added Identity table to admin pages.

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from app.models import Player
+from app.models import Player, Identity
 
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('username', 'bnet_id', 'mmr', 'race')
     readonly_fields = ('created_at', 'modified_at')
+    search_fields = ('username', 'bnet_id', 'identity__alias')
+
+
+@admin.register(Identity)
+class IdentityAdmin(admin.ModelAdmin):
+    list_display = ('alias', 'first_name', 'last_name')
+    readonly_fields = ('created_at', 'modified_at')
+    search_fields = ('alias', 'first_name', 'last_name')
