f77b56253c55cb37b86c45419c39ddf9fba00b47
jason <jteague9@gatech.edu>
2019-06-18 14:24:20 -0700

    Wrap players.create_player with try catch in case of invalid player data.

--- app/src/parse.py
+++ app/src/parse.py
@@ -17,19 +17,22 @@ def parse_ladder(response_json, region) -> List[Player]:
                 bnet_id = j.get('character_link', {}).get('battle_tag')
                 profile_id = j.get('character_link', {}).get('id')
                 clan_tag = j.get('clan_link', {}).get('clan_tag')
-                ladder_list.append(Player.players.create_player(
-                    realm=realm,
-                    region=region.upper(),
-                    rank=int(rank),
-                    username=character_name,
-                    bnet_id=bnet_id,
-                    race=race,
-                    mmr=i.get('rating'),
-                    wins=i.get('wins'),
-                    losses=i.get('losses'),
-                    clan=clan_tag,
-                    profile_id=profile_id,
-                    modified_at=timezone.now()
-                ))
+                try:
+                    ladder_list.append(Player.players.create_player(
+                        realm=realm,
+                        region=region.upper(),
+                        rank=int(rank),
+                        username=character_name,
+                        bnet_id=bnet_id,
+                        race=race,
+                        mmr=i.get('rating'),
+                        wins=i.get('wins'),
+                        losses=i.get('losses'),
+                        clan=clan_tag,
+                        profile_id=profile_id,
+                        modified_at=timezone.now()
+                    ))
+                except:
+                    pass
 
     return ladder_list
