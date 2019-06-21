5a9b39593daf717fe3befa09e9b1002bd4e650b5
Tristan Ohlson <tsohlson@gmail.com>
2019-06-20 20:54:32 -0400

    Sends errors to discord.

--- sc2ladder/settings/production.py
+++ sc2ladder/settings/production.py
@@ -4,9 +4,51 @@ import django_heroku
 
 from .common import *
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
 
 DEBUG = False
+
+DISCORD_WEBHOOK = os.environ['DISCORD_WEBHOOK']
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'discord_logger': {
+            'level': 'ERROR',
+            'class': 'sc2ladder.discord_logger.DiscordExceptionHandler',
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django': {
+            'handlers': ['console', 'discord_logger'],
+            'level': 'INFO',
+        }
+    }
+}
 
 DB_BATCH_SIZE = 1000
 url = urlparse.urlparse(os.environ['DATABASE_URL'])
