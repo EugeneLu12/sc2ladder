db3160ce3bdaecfd959a5446abbcd9a7860eb082
Tristan Ohlson <tohlson@squareup.com>
2020-01-03 21:05:32 -0500

    Add script to delete data older than 30 days old

---
+++ app/management/commands/prune_old_data.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+
+from django.core.management.base import BaseCommand
+
+from app.models.player import Player
+
+
+class Command(BaseCommand):
+    help = 'Deletes any player data that is older than 30 days'
+
+    def handle(self, *args, **options):
+        now = datetime.now()
+        thirty_days_ago = now - timedelta(days=30)
+        Player.players.filter(updated_at__lte=thirty_days_ago).delete()
