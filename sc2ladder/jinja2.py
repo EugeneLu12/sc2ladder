b6e85896d9052a49f472e858f49905d0349916b4
jason <jteague9@gatech.edu>
2020-03-19 00:30:37 -0700

    Changed headers and added a function to show relative updated time.

--- sc2ladder/jinja2.py
+++ sc2ladder/jinja2.py
@@ -1,6 +1,25 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from jinja2 import Environment
+from django.utils import timezone
+
+
+def updated_ago(mod_time):
+    d = timezone.now() - mod_time
+    if d.days > 1:
+        return f"{d.days} days ago"
+    elif d.days == 1:
+        return "1 day ago"
+    elif d.seconds > 7200:
+        return f"{int(d.seconds/3600)} hours ago"
+    elif d.seconds > 3600:
+        return "1 hour ago"
+    elif d.seconds > 120:
+        return f"{int(d.seconds/60)} minutes ago"
+    elif d.seconds > 60:
+        return "1 minute ago"
+    else:
+        return f"{d.seconds} seconds ago"
 
 
 def environment(**options):
@@ -8,5 +27,6 @@ def environment(**options):
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
+        'updated_ago': updated_ago
     })
     return env
