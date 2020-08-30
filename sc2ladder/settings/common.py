400463974dc59700f1db845d6e3ff25b89e1edb8
William Stewart <willstewartcs@gmail.com>
2020-08-30 14:44:42 -0400

    Use REDIS_URL env variable.

--- sc2ladder/settings/common.py
+++ sc2ladder/settings/common.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.environ["REDIS_URL"],
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
         },
